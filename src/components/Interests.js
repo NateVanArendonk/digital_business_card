166977cea820d66cb255be1220094bef9ae225a7
Nate VanArendonk <nathan.vanarendonk@gmail.com>
2023-05-22 21:50:16 -0700

    Add about section to digital business card

---
+++ src/components/Interests.js
@@ -0,0 +1,8 @@
+import React from "react"
+import photo from "../images/headshot.jpg"
+
+export default function PhotoHeader() {
+    return (
+        <div>Interests</div>
+    )
+}
---
+++ src/components/Main.js
@@ -0,0 +1,46 @@
+import React from "react"
+import photo from "../images/headshot.jpg"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons";
+
+export default function Main() {
+    return (
+        <div>
+            <header>
+                <img src={photo} className="photo"></img>
+            </header>
+            <div className="main-name">
+                Nathan VanArendonk
+            </div>
+            <div className="main-job">
+                Software Engineer
+                  <FontAwesomeIcon icon={["far", "coffee"]} />
+            </div>
+            <div className="main-location">
+                Bellingham, WA
+            </div>
+            <div className="main--buttons">
+                <address>
+                    <a href="mailto:nathan.vanarendonk@gmail.com">
+                        {" "}
+                        <button className="button button--email">
+                            <FontAwesomeIcon icon={faEnvelope} className="info--icon" />
+                            Email
+                        </button>
+                    </a>
+                </address>
+                <a
+                    href="https://github.com/NateVanArendonk/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <button className="button button--github">
+                        <FontAwesomeIcon icon={faGithub} className="info--icon" />
+                        GitHub
+                    </button>
+                </a>
+            </div>
+        </div>
+    )
+}
