5b8ba5154bb1d073e2dd9a6cf91fa080e7217ca7
Nate VanArendonk <nathan.vanarendonk@gmail.com>
2023-05-25 22:07:08 -0700

    Initial add footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,25 @@
 import React from "react"
+import { faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 
 export default function Footer() {
     return (
-        <footer>Footer</footer>
+        <footer>
+            <a
+                href="https://github.com/xdelmo"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                <FontAwesomeIcon icon={faGithub} className="info--icon" />
+            </a>
+            <a
+                href="https://www.linkedin.com/in/emanueledelmonte/"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                <FontAwesomeIcon icon={faLinkedin} className="info--icon" />
+            </a>
+        </footer>
     )
 }
