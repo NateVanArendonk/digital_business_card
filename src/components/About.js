d8da0bfc3e616f1f90f64259fc490b2fd9d22785
Nate VanArendonk <nathan.vanarendonk@gmail.com>
2023-05-25 20:32:48 -0700

    Add about text to card

--- src/components/About.js
+++ src/components/About.js
@@ -2,6 +2,14 @@ import React from "react"
 
 export default function About() {
     return (
-        <div>About</div>
+        <div>
+            <h3 className="about-header">
+                About
+            </h3>
+            <div className="about-text">
+                I am a backend developer specializing in seamless user experiences and efficient data processing. 
+                While primarily focused on backend development, I am actively expanding my front-end proficiency with React.
+            </div>
+        </div>
     )
 }
--- src/components/Main.js
+++ src/components/Main.js
@@ -2,7 +2,7 @@ import React from "react"
 import photo from "../images/headshot.jpg"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
-import { faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
 
 export default function Main() {
     return (
