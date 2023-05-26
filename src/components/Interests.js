e38adad8f380a4babde7b7353323921ab9c05408
Nate VanArendonk <nathan.vanarendonk@gmail.com>
2023-05-25 21:53:31 -0700

    Add interests section

--- src/components/Interests.js
+++ src/components/Interests.js
@@ -3,6 +3,12 @@ import photo from "../images/headshot.jpg"
 
 export default function PhotoHeader() {
     return (
-        <div>Interests</div>
-    )
+        <div>
+            <h3 className="interests-header">
+                Interests
+            </h3>
+            <div className="interests-text">
+                Mountain Biking. Kiteboarding. Skiing. Synthesizers. Coffee. Dachshunds.
+            </div>
+        </div>    )
 }
